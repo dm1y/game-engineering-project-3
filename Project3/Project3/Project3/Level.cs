8bbec563ba3ea49484b73cd39e27032224d92b68
dm1y <dianamly@live.com>
2014-11-21 01:16:14 -0800

    Added pseudo code and logic to level class.

--- Project3/Project3/Project3/Level.cs
+++ Project3/Project3/Project3/Level.cs
@@ -11,6 +11,44 @@ namespace Project3
         /* TODO: Move level creation logic here instead of having it in World 
          TODO: Use StreamReaders to create the levels via ascii 
          TODO: to access texts/textures, specify directory or folder name/then the actual name 
-         Ex: "Levels/level1.txt" when doing Content.Load , this will keep things organized */
+         Ex: "Levels/level1.txt" when doing Content.Load , this will keep things organized
+         * 
+         
+         Pseudo logic / code ;
+         * 
+         This will be in World class: 
+         string levelPath = "Content/Levels/ " + index of level specifying type of level ".txt"
+         using (Stream file = TitleContainer.OpenStream(levelPath))
+                level = new Level(file, levelIndex);
+         
+
+         ** use this in the load method 
+         List<string> line = new List<string>();
+         using (StreamReader reader = new StreamReader(file))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    lines.Add(line);
+         * // should probably check to make sure the width of each text file is the same, will figure out logic for that  later 
+                    line = reader.ReadLine();
+                }
+         * 
+         * 
+         * use the line.Count information to initialize the size of the grid 
+         * 
+         * use colin's nested for loop here, 
+         * but when reaching the switch statement, create a new tile based on the input of the ascii . 
+         * 
+         * also redo some logic for map tile that has "subclasses"/different types of tiles created
+         * 
+         * And this should populate the grid. 
+         * 
+         * Transitions will still remain the world class.
+         * 
+         * 
+            }*/
+
+
     }
 }
