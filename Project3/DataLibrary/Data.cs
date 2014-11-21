fd4fe2ed36a31a925d717d425b74585b68494ebc
dm1y <dianamly@live.com>
2014-11-21 00:56:48 -0800

    Added data class to make things data driven later

---
+++ Project3/DataLibrary/Data.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Project3
+{
+    public class Data
+    {
+         public String texture;
+         public Vector2 position;
+         public Boolean isCollidable;
+         public Boolean isTransition;
+         public Boolean isDangerous;
+         public Boolean isInteract;
+    }
+}
